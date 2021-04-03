b4a2a3e7344d15cee2b21b03e0702ed29d0d057f
GoktugEk <goktug.ekinci@metu.edu.tr>
2021-04-03 20:34:58 +0300

    Comment update on curve fitting code

--- curve_fitting.py
+++ curve_fitting.py
@@ -14,6 +14,12 @@ warnings.simplefilter("ignore")
 
 
 def clear_zeros(lis):
+    """
+
+    :param lis:  an array or list with integers
+    turns every 0 into 0.1s
+    :return: list with 0.1 s instead of 0
+    """
     for i in range(len(lis)):
         if lis[i] == 0:
             lis[i] = 0.1
@@ -22,32 +28,39 @@ def clear_zeros(lis):
     return lis
 
 def func(x,a,b,c,d,e,f):
+    """
+    model function for curve fitting
+    """
     return int(a) * x + b * np.square(x) + c + d*np.exp(-((x-e)/f)**2)
 
 
 df = pd.read_csv("owid-covid-data.csv")
 
-cnts = df.location.unique()
+cnts = df.location.unique() #country names
 
 
-for cnt in cnts:
+for cnt in cnts: #for every country
     cnt_df = df[df["location"] == cnt]
 
     fts = ['new_cases', 'new_deaths', 'new_tests']
     names = {'new_cases' : 'Case', 'new_deaths' : 'Death', 'new_tests' : 'Test'}
-    for ft in fts:
-        arr = cnt_df[ft].dropna().to_numpy()
-        arr = clear_zeros(arr)
+
+    for ft in fts: #for every feature
+
+        arr = cnt_df[ft].dropna().to_numpy() #droping nan values
+        arr = clear_zeros(arr) #cleaning the zero values
 
         if len(arr) < 20:
             break
 
         days = [x for x in range(1, len(arr) + 1)]
 
-        try:
+        try: #it has been put since if the correct parameters not found, function gives an error. We want to skip it
             params,_ = curve_fit(func, days, arr, maxfev=10000000)
         except:
             break
+
+        #plotting both curves
         plt.plot(days, arr, 'b.', label=cnt)
         days = [x for x in range(1, len(arr) + 31)]
         plt.plot(days, func(days, *params), 'r-', label='Fit')
